32f34a1b996554cc239d9518f9a4eee64c721a72
Kai-Hsin, Hung <danielkai890802@gmail.com>
2024-05-28 18:12:21 +0800

    matlabplot

--- main2.py
+++ main2.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import openpyxl
 from openpyxl.styles import Font
-from openpyxl.styles import Alignment
+from openpyxl.styles.alignment import Alignment
+import matplotlib.pyplot as plt 
 
 # Read csv file
 measure_data = pd.read_csv("Measure_data.csv", usecols=[2])
@@ -27,9 +28,18 @@ for transit in range(1, measure_data, 1):
     sheet.cell((transit + 1),2).value = outcome
 
 sheet.cell(1,2).value = "Time"
-sheet.cell(1,2).font = Font(bold = True, Alignment=(Alignment(horizontal="center")))
-sheet.cell(1.2).alignment = Alignment
+sheet.cell(1,2).font = Font(bold = True)
+sheet["B1"].alignment = Alignment(horizontal= "center")
+wb_open.save("test_excel.xlsx")
 
-
+read_excel = pd.read_excel("test_excel.xlsx")
+x_axis = read_excel["Time"]
+y_axis = read_excel["Distance"]
+plt.plot(x_axis, y_axis, color = "green")
+plt.xlabel("Time(s)", fontsize = "12")
+plt.ylabel("Distance(mm)", fontsize = "12")
+plt.title("Actuator", fontsize = "15")
+plt.ylim(-7,2)
+plt.show()
 
 
