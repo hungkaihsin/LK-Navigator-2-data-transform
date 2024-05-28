5848c82cfc915ae1c6801ec080d6381d367f3f03
Kai-Hsin, Hung <danielkai890802@gmail.com>
2024-05-28 17:03:38 +0800

    adjust

--- main2.py
+++ main2.py
@@ -1,31 +1,35 @@
 import pandas as pd
+import openpyxl
+from openpyxl.styles import Font
+from openpyxl.styles import Alignment
 
 # Read csv file
-measure_data = pd.read_csv("Measure_data.csv")
-print(measure_data.head())
+measure_data = pd.read_csv("Measure_data.csv", usecols=[2])
 
-header_list = ["Categroy", "Time", "Distance"]
+header_list = ["Distance"]
 
 # Add Header
-measure_data.to_csv("Measure_data2.csv", header= header_list, index= False)
-
-
-usecols = ["Distance"]
-
-measure_data2 = pd.read_csv("Measure_data2.csv", index_col=0, usecols=usecols)
-print(measure_data2.head())
+measure_data.to_excel("test_excel.xlsx", header= header_list, index= False)
 
 
 
+wb_open = openpyxl.load_workbook("test_excel.xlsx")
+sheet = wb_open.worksheets[0]
 measure_time = 500
 measure_data = 100000
 transition = float(measure_time / measure_data)
 
-print(transition)
+
 
 for transit in range(1, measure_data, 1):
     count = transit
-    calculate =  count * transition
-    
+    outcome=  count * transition
+    sheet.cell((transit + 1),2).value = outcome
 
-    
+sheet.cell(1,2).value = "Time"
+sheet.cell(1,2).font = Font(bold = True, Alignment=(Alignment(horizontal="center")))
+sheet.cell(1.2).alignment = Alignment
+
+
+
+
